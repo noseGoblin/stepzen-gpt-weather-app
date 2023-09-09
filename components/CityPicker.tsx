c02fd936569c69b4d8fd8e957aeec0e20907723a
Jacob Mompean <jaymompean@gmail.com>
2023-09-09 18:10:42 -0500

    getAllCountries by lat / long / iso

--- components/CityPicker.tsx
+++ components/CityPicker.tsx
@@ -1,6 +1,16 @@
 'use client';
 
 import { Country, City } from 'country-state-city';
+import { Select } from '@tremor/react';
+
+const options = Country.getAllCountries().map((country) => ({
+  label: {
+    latitude: country.latitude,
+    longitude: country.longitude,
+    isoCode: country.isoCode,
+  },
+  value: country.isoCode,
+}));
 
 function CityPicker() {
   return <div>CityPicker</div>;
