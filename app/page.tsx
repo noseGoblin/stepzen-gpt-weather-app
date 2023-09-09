a18355af54258a4a71db337ed56b64b25e1866d1
Jacob Mompean <jaymompean@gmail.com>
2023-09-09 18:01:31 -0500

    set home bg; make main card responsive; add CityPicker Card

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,19 @@ import { Card, Divider, Subtitle, Text } from '@tremor/react';
 
 export default function Home() {
   return (
-    <main className='text-6xl'>
-      <Card className=''>
+    <div className='min-h-screen bg-gradient-to-br from-[#394F68] to-[#183B7E] p-10 flex flex-col justify-center'>
+      <Card className='max-w-4xl mx-auto'>
         <Text className='text-6xl font-bold text-center mb-10'>Weather AI</Text>
         <Subtitle className='text-xl text-center'>
           Powered by OpenAI, Nex.js 13.4, TailwindCSS, Tremor 2.0 + More!
         </Subtitle>
 
         <Divider className='my-10' />
+
+        <Card className='bg-gradient-to-br from-[#394F68] to-[#183B7E]'>
+          {/* CityPicker */}
+        </Card>
       </Card>
-    </main>
+    </div>
   );
 }
