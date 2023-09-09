7ee1ea5a003bb979324df5a2e32adecc08be6dde
Jacob Mompean <jaymompean@gmail.com>
2023-09-09 17:55:36 -0500

    style first Card and remove Tremor default font sizing

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
-import { Card, Subtitle, Text } from '@tremor/react';
+import { Card, Divider, Subtitle, Text } from '@tremor/react';
 
 export default function Home() {
   return (
-    <main className=''>
+    <main className='text-6xl'>
       <Card className=''>
-        <Text>Weather AI</Text>
-        <Subtitle>
+        <Text className='text-6xl font-bold text-center mb-10'>Weather AI</Text>
+        <Subtitle className='text-xl text-center'>
           Powered by OpenAI, Nex.js 13.4, TailwindCSS, Tremor 2.0 + More!
         </Subtitle>
+
+        <Divider className='my-10' />
       </Card>
     </main>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -92,12 +92,12 @@ const config: Config = {
         'tremor-default': '0.5rem',
         'tremor-full': '9999px',
       },
-      fontSize: {
-        'tremor-label': ['0.75rem'],
-        'tremor-default': ['0.875rem', { lineHeight: '1.25rem' }],
-        'tremor-title': ['1.125rem', { lineHeight: '1.75rem' }],
-        'tremor-metric': ['1.875rem', { lineHeight: '2.25rem' }],
-      },
+      // fontSize: {
+      //   'tremor-label': ['0.75rem'],
+      //   'tremor-default': ['0.875rem', { lineHeight: '1.25rem' }],
+      //   'tremor-title': ['1.125rem', { lineHeight: '1.75rem' }],
+      //   'tremor-metric': ['1.875rem', { lineHeight: '2.25rem' }],
+      // },
     },
   },
   safelist: [
