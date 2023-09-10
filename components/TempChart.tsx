24e8c5327ac27773f358b7b80624e6cb0bd9a3da
Jacob Mompean <jaymompean@gmail.com>
2023-09-10 00:10:16 -0500

    fix TempChart slice to show 24hr chart

--- components/TempChart.tsx
+++ components/TempChart.tsx
@@ -7,14 +7,14 @@ type Props = {
 };
 
 function TempChart({ results }: Props) {
-  const hourly = results?.hourly.time.map((time) =>
-    new Date(time)
-      .toLocaleString('en-US', {
+  const hourly = results?.hourly.time
+    .map((time) =>
+      new Date(time).toLocaleString('en-US', {
         hour: 'numeric',
         hour12: false,
       })
-      .slice(0, 24)
-  );
+    )
+    .slice(0, 24);
 
   const data = hourly.map((hour, i) => ({
     'time': Number(hour),
