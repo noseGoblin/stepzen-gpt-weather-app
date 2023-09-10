a0a70eabb5df18379a22821663dc6ebd7bc8c840
Jacob Mompean <jaymompean@gmail.com>
2023-09-09 22:20:19 -0500

    add and style remaining Wind StatCards

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -43,37 +43,59 @@ async function WeatherPage({ params: { city, lat, long } }: Props) {
             </p>
           </div>
 
-          <div>
+          <div className='m-2 mb-10'>
             <CalloutCard message='This is where GPT-4 Summary will go!' />
           </div>
 
-          <div>
+          <div className='grid grid-cols-1 xl:grid-cols-2 gap-5 m-2'>
             <StatCard
               title='Maximum Temperature'
-              metric={`${results.daily.temperature_2m_max[0].toFixed(1)}°C`}
+              metric={`${results.daily.temperature_2m_max[0].toFixed(1)}°`}
               color='yellow'
             />
 
             <StatCard
               title='Minimum Temperature'
-              metric={`${results.daily.temperature_2m_min[0].toFixed(1)}°C`}
+              metric={`${results.daily.temperature_2m_min[0].toFixed(1)}°`}
               color='green'
             />
-          </div>
 
-          <div>
-            <StatCard
-              title='UV Index'
-              metric={results.daily.uv_index_max[0].toFixed(1)}
-              color='rose'
-            />
-            {Number(results.daily.uv_index_max[0].toFixed(1)) > 5 && (
-              <CalloutCard
-                message={'The UV is high today, be sure to wear SPF!'}
-                warning
+            <div>
+              <StatCard
+                title='UV Index'
+                metric={results.daily.uv_index_max[0].toFixed(1)}
+                color='rose'
               />
-            )}
+              {Number(results.daily.uv_index_max[0].toFixed(1)) > 5 && (
+                <CalloutCard
+                  message={'The UV is high today, be sure to wear SPF!'}
+                  warning
+                />
+              )}
+            </div>
+
+            <div className='flex space-x-5'>
+              <StatCard
+                title='Wind Speed'
+                metric={`${results.current_weather.windspeed.toFixed(1)} m/s`}
+                color='cyan'
+              />
+
+              <StatCard
+                title='Wind Direction'
+                metric={`${results.current_weather.winddirection.toFixed(1)}°`}
+                color='fuchsia'
+              />
+            </div>
           </div>
+        </div>
+
+        <hr className='mb-5' />
+
+        <div className='space-y-3'>
+          {/* TempChart */}
+          {/* RainChart */}
+          {/* HumidityChart */}
         </div>
       </div>
     </div>
