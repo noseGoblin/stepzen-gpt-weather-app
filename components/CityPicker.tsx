77d26b38b4353a765db64bfd7f103bc33be24529
Jacob Mompean <jaymompean@gmail.com>
2023-09-09 19:14:20 -0500

    fix heroicons versioning issue and add globe icon

--- components/CityPicker.tsx
+++ components/CityPicker.tsx
@@ -4,6 +4,7 @@ import { Country, City } from 'country-state-city';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import Select from 'react-select';
+import { GlobeIcon } from '@heroicons/react/solid';
 
 type option = {
   value: {
@@ -44,8 +45,9 @@ function CityPicker() {
   };
 
   return (
-    <div>
-      <div>
+    <div className='space-y-4'>
+      <div className='flex items-center space-x-2 text-white/80'>
+        <GlobeIcon className='h-5 w-5 text-white' />
         <label htmlFor='country'>Country</label>
       </div>
       <Select
