3c0c9e6d9b228331786901d8a01782586438595c
Jacob Mompean <jaymompean@gmail.com>
2023-09-09 19:39:09 -0500

    set WetherPage props

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -1,4 +1,12 @@
-function WeatherPage() {
+type Props = {
+  params: {
+    city: string;
+    lat: string;
+    long: string;
+  };
+};
+
+function WeatherPage({ params: { city, lat, long } }: Props) {
   return <div>Welcome to the WeatherPage</div>;
 }
 
