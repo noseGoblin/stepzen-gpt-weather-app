d652ef8a856ec77f5136b8cfce886a47d1cbd5f9
Jacob Mompean <jaymompean@gmail.com>
2023-09-09 21:33:15 -0500

    add client to pages and print all weather data to the console

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -1,3 +1,6 @@
+import { getClient } from '@/apollo-client';
+import fetchWeatherQuery from '@/graphql/queries/fetchWeatherQueries';
+
 type Props = {
   params: {
     city: string;
@@ -6,7 +9,23 @@ type Props = {
   };
 };
 
-function WeatherPage({ params: { city, lat, long } }: Props) {
+async function WeatherPage({ params: { city, lat, long } }: Props) {
+  const client = getClient();
+
+  const { data } = await client.query({
+    query: fetchWeatherQuery,
+    variables: {
+      current_weather: 'true',
+      latitude: lat,
+      longitude: long,
+      timezone: 'GMT',
+    },
+  });
+
+  const results: Root = data.myQuery;
+
+  console.log(results);
+
   return (
     <div>
       Welcome to the WeatherPage: {city} {lat} {long}
