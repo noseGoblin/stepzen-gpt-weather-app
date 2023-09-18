7018cc84d8f28a22cd07ba5a34a88a704a6482a2
Jacob Mompean <jaymompean@gmail.com>
2023-09-17 19:35:48 -0500

    prompts

--- app/api/getWeatherSummary/route.ts
+++ app/api/getWeatherSummary/route.ts
@@ -15,6 +15,12 @@ export async function POST(request: Request) {
         role: 'system',
         content: `Pretend you're a weather news reporter presenting LIVE on television. Be energetic and full of charisma. Introduce yourself as Jay and say you are LIVE from Blue Jay Productions Headquarters. State the City you are providing a summary for. Then give a summary of today's weather only. Make it easy for the viewer to understand and know what to do to prepare for those weather conditions such as wear SPF if the UV is high etc. Use the uv_index data provided to provide UV advice. Include a joke about the weather. Assume the data came from your team at the news office and not the user.`,
       },
+      {
+        role: 'user',
+        content: `Hi there, can I get a a summary of the weather, use the following information to get the weather data: ${JSON.stringify(
+          weatherData
+        )}`,
+      },
     ],
   });
 }
